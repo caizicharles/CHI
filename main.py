e1ce327b1237407b0a462637fc503b1f2a9a3a3f
caiz <zc348@cam.ac.uk>
2024-06-18 10:10:11 +0100

    save with json

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
-from utils import get_args
+from utils import *
 from dataset import process_dataset
+
+import time
 
 
 def main(args):
 
+    # start = time.time()
+    
+    # dataset = load_with_pickle(args.save_data_path, 'mimic-iv.pickle')
+
+    # end = time.time()
+    # print(dataset)
+    # print(start-end)
+
+    # exit()
+
+    a = time.time()
+
     dataset = process_dataset(args.dataset)
-    print(dataset.__dict__)
-    print(dataset.info())
-    print(dataset.stat())
+
+    b = time.time()
+    print(b-a)
+
+    save_with_json(dataset, args.save_data_path, 'mimic-iv.json')
+
+    c = time.time()
+    print(c-b)
+
+    dataset = load_with_json(args.save_data_path, 'mimic-iv.json')
+
+    d = time.time()
+    print(d-c)
+    print(dataset)
+
     
     return
 
 if __name__ == '__main__':
     args = get_args()
-    print(args)
     main(args=args)
--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,9 @@
 import pickle
+import json
 import os.path as osp
 
 
-def save_processed_dataset(dataset: object, save_path: str, file_name: str):
+def save_with_pickle(file: object, save_path: str, file_name: str):
 
     if not file_name.lower().endswith('.pickle'):
         file_name += '.pickle'
@@ -10,12 +11,12 @@ def save_processed_dataset(dataset: object, save_path: str, file_name: str):
     file_path = osp.join(save_path, file_name)
 
     with open(file_path, 'wb') as f:
-        pickle.dump(dataset, f)
+        pickle.dump(file, f)
 
     return
 
 
-def load_processed_dataset(save_path: str, file_name: str):
+def load_with_pickle(save_path: str, file_name: str):
 
     if not file_name.lower().endswith('.pickle'):
         file_name += '.pickle'
@@ -24,6 +25,36 @@ def load_processed_dataset(save_path: str, file_name: str):
     assert osp.isfile(file_path), f'Dataset save path: {file_path} is invalid.'
 
     with open(file_path, "rb") as f:
-        dataset = pickle.load(f)
+        file = pickle.load(f)
 
-    return dataset
+    return file
+
+
+def save_with_json(file, save_path: str, file_name: str):
+
+    if not file_name.lower().endswith('.json'):
+        file_name += '.json'
+
+    file_path = osp.join(save_path, file_name)
+
+    if type(file) == object:
+        file = file.__dict__    
+
+    with open(file_path, 'w') as f:
+        json.dump(file, f)
+
+    return
+
+
+def load_with_json(save_path: str, file_name: str):
+
+    if not file_name.lower().endswith('.json'):
+        file_name += '.json'
+
+    file_path = osp.join(save_path, file_name)
+    assert osp.isfile(file_path), f'Dataset save path: {file_path} is invalid.'
+
+    with open(file_path, "rb") as f:
+        file = json.load(f)
+
+    return file
