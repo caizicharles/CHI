1e58de17363699404da1d0c267fabff153fbe5c8
caizicharles <zc348@cam.ac.uk>
2024-06-20 11:14:10 +0100

    new args for training

--- utils/args.py
+++ utils/args.py
@@ -5,11 +5,22 @@ import yaml
 def get_args():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--dataset', type=str, default='mimic-iv')
+    parser.add_argument('--task', type=str, default='')
+    parser.add_argument('--seed', type=int, default=0)
+
+    # Training Setting
+    parser.add_argument('--train_proportion', type=float, default=0.8)
+    parser.add_argument('--val_proportion', type=float, default=0.1)
+    parser.add_argument('--test_proportion', type=float, default=0.1)
+    parser.add_argument('--batch_size', type=int, default=3)
+
     # Data Paths
+    parser.add_argument('--raw_data_path', type=str, default='')
     parser.add_argument('--save_data_path', type=str, default='')
 
     # Model Params
-    parser.add_argument('--dataset', type=str, default='mimic-iv')
+    
 
     # Config File
     config_parser = argparse.ArgumentParser(description='Algorithm Config', add_help=False)
@@ -27,6 +38,8 @@ def get_args():
         parser.set_defaults(**cfg)
 
     args = parser.parse_args(remaining)
+
+    assert args.train_proportion + args.val_proportion + args.test_proportion == 1., 'train-val-test split should sum to 1'
 
     return args
 
