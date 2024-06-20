29bf23b3108235b09c820f96d7b17c04e47c44f0
caizicharles <zc348@cam.ac.uk>
2024-06-20 11:02:26 +0100

    init file bug fix

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
 import pickle
-import json
+import csv
 import os.path as osp
 
 
@@ -30,31 +30,29 @@ def load_with_pickle(save_path: str, file_name: str):
     return file
 
 
-def save_with_json(file, save_path: str, file_name: str):
+def save_with_csv(file: dict, header: list, save_path: str, file_name: str):
 
-    if not file_name.lower().endswith('.json'):
-        file_name += '.json'
+    if not file_name.lower().endswith('.csv'):
+        file_name += '.csv'
 
     file_path = osp.join(save_path, file_name)
 
-    if type(file) == object:
-        file = file.__dict__    
-
-    with open(file_path, 'w') as f:
-        json.dump(file, f)
-
-    return
+    with open(file_path, 'w') as file:
+        writer = csv.DictWriter(file, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(file)
 
 
-def load_with_json(save_path: str, file_name: str):
+def read_csv_file(save_path: str, file_name: str):
 
-    if not file_name.lower().endswith('.json'):
-        file_name += '.json'
+    if not file_name.lower().endswith('.csv'):
+        file_name += '.csv'
 
     file_path = osp.join(save_path, file_name)
     assert osp.isfile(file_path), f'Dataset save path: {file_path} is invalid.'
 
-    with open(file_path, "rb") as f:
-        file = json.load(f)
+    with open(file_path, "r") as f:
+        file = csv.reader(f)
+        file = list(file)
 
     return file
