268211de51a580cbf2d306d84bc3beaee3eb0285
caizicharles <zc348@cam.ac.uk>
2024-06-28 17:19:10 +0100

    added PMA from set transformer

--- model/modules.py
+++ model/modules.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+
 # Credit to SetTransformer
 
 
@@ -10,6 +11,7 @@ class MAB(nn.Module):
 
     def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
         super(MAB, self).__init__()
+
         self.dim_V = dim_V
         self.num_heads = num_heads
         self.fc_q = nn.Linear(dim_Q, dim_V)
@@ -41,6 +43,7 @@ class SAB(nn.Module):
 
     def __init__(self, dim_in, dim_out, num_heads, ln=False):
         super(SAB, self).__init__()
+
         self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln)
 
     def forward(self, X):
@@ -51,6 +54,7 @@ class ISAB(nn.Module):
 
     def __init__(self, dim_in, dim_out, num_heads, num_inds, ln=False):
         super(ISAB, self).__init__()
+
         self.I = nn.Parameter(torch.Tensor(1, num_inds, dim_out))
         nn.init.xavier_uniform_(self.I)
         self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln)
@@ -59,3 +63,16 @@ class ISAB(nn.Module):
     def forward(self, X):
         H = self.mab0(self.I.repeat(X.size(0), 1, 1), X)
         return self.mab1(X, H)
+
+
+class PMA(nn.Module):
+
+    def __init__(self, dim, num_heads, num_seeds, ln=False):
+        super(PMA, self).__init__()
+
+        self.S = nn.Parameter(torch.Tensor(1, num_seeds, dim))
+        nn.init.xavier_uniform_(self.S)
+        self.mab = MAB(dim, dim, dim, num_heads, ln=ln)
+
+    def forward(self, X):
+        return self.mab(self.S.repeat(X.size(0), 1, 1), X)
