8d6f1d0484645777bc2ff69c3518f148e8ee71e4
caizicharles <zc348@cam.ac.uk>
2024-06-28 17:18:13 +0100

    modified for tensorboardX

--- utils/misc.py
+++ utils/misc.py
@@ -1,16 +1,23 @@
 import logging
+import tensorboardX
+import os.path as osp
+import time
 
 
-def init_logger():
+def init_logger(args):
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
 
-    logger.addHandler(console_handler)
+    logging.basicConfig(level=logging.INFO, handlers=[console_handler])
 
-    return logger
+    writer = tensorboardX.SummaryWriter(log_dir=osp.join(args.log_dir, 'tensorboard', args.start_time))
+
+    return writer
+
+
+def get_time_str():
+    return time.strftime('%Y%m%d_%H%M%S', time.localtime())
