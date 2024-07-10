3538efcde4aaa402b8f4e999df2a74be8ffde3de
caizicharles <zc348@cam.ac.uk>
2024-07-10 16:34:41 +0100

    model for mortality, attn mask changes

--- model/model.py
+++ model/model.py
@@ -5,45 +5,62 @@ from torch_geometric.nn import GATConv
 from .modules import *
 
 
-class TimeTransformet(nn.Module):
+class PredictionHead(nn.Module):
 
     def __init__(self,
-                 input_dim,
-                 hidden_dim,
-                 output_dim,
-                 num_heads=4,
-                 depth=2,
-                 ln=False):
+                 attn_hidden_dim,
+                 mlp_input_dim,
+                 mlp_hidden_dim,
+                 mlp_output_dim,
+                 head_num=1,
+                 ln=False,
+                 mlp_act_layer=nn.GELU):
         super().__init__()
 
-        self.attn = None
+        self.Q = nn.Parameter(torch.Tensor(1, 1, mlp_input_dim))
 
-    def forward(self, X, embed):
+        self.attn = MAB(attn_hidden_dim, attn_hidden_dim, mlp_input_dim, head_num, ln=ln)
+        self.mlp = nn.Linear(mlp_input_dim, mlp_output_dim)
+        # self.mlp = MLP(mlp_input_dim, mlp_hidden_dim, mlp_output_dim, mlp_act_layer)
 
+    def forward(self, X):
+        X = self.attn(self.Q.repeat(X.size(0), 1, 1), X)
+        X = X.squeeze(-2)  # (B,D)
+        X = self.mlp(X)  # (B,1)
+        return X
+
+
+class TimeTransformer(nn.Module):
+
+    def __init__(self, hidden_dim, num_heads=4, depth=2, ln=False):
+        super().__init__()
+
+        self.attn = nn.ModuleList([SAB(2 * hidden_dim, 2 * hidden_dim, num_heads, ln=ln) for _ in range(depth)])
+
+    def forward(self, X, visit_x, embed, attn_mask=None):
+        embed = embed.repeat(1, 1, 2)  # (B,V,2D)
+        X = torch.cat((X, visit_x), dim=-1)  # (B,V,2D)
+
+        for _, block in enumerate(self.attn):
+            X = block(X + embed, attn_mask)
         return X
 
 
 class PrototypeLearner(nn.Module):
 
-    def __init__(self,
-                 input_dim,
-                 hidden_dim,
-                 output_dim,
-                 num_heads=4,
-                 depth=2,
-                 ln=False):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_heads=4, depth=2, ln=False):
         super().__init__()
 
         # self.attn = nn.ModuleList([MAB(input_dim, input_dim, hidden_dim, num_heads, ln=ln)
         #                            for _ in range(depth)])
         self.attn = MAB(input_dim, input_dim, hidden_dim, num_heads, ln=ln)
-        self.ffn = nn.Linear(hidden_dim, output_dim)
+        self.fc = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, X, P):
         X = self.attn(X, P)
         # for _, block in enumerate(self.attn):
         #     P = block(X, P)
-        X = self.ffn(X)
+        X = self.fc(X)
         return X
 
 
@@ -68,14 +85,14 @@ class SetTransformer(nn.Module):
             PMA(dim_hidden, num_heads, num_outputs, ln=ln),
         ])
         self.dec.extend([SAB(dim_hidden, dim_hidden, num_heads, ln=ln) for _ in range(decoder_depth)])
-        self.proj = nn.Linear(dim_hidden, dim_output)
+        self.fc = nn.Linear(dim_hidden, dim_output)
 
     def forward(self, X, attn_mask=None):
         for _, block in enumerate(self.enc):
             X = block(X, attn_mask)
         for _, block in enumerate(self.dec):
             X = block(X, attn_mask)
-        return self.proj(X)
+        return self.fc(X)
 
 
 class OurModel(nn.Module):
@@ -93,7 +110,6 @@ class OurModel(nn.Module):
                  proto_hidden_dim,
                  proto_out_dim,
                  time_trans_hidden_dim,
-                 time_trans_out_dim,
                  out_dim,
                  gnn_layer=1,
                  proto_num=5,
@@ -105,8 +121,7 @@ class OurModel(nn.Module):
                  proto_head_num=4,
                  proto_depth=2,
                  time_head_num=4,
-                 time_encoder_depth=2,
-                 time_decoder_depth=2,
+                 time_depth=2,
                  node_embed=None,
                  edge_embed=None,
                  freeze=False):
@@ -117,13 +132,13 @@ class OurModel(nn.Module):
         self.num_edges = num_edges
         self.gnn_layer = gnn_layer
         self.gnn_hidden_dim = gnn_hidden_dim
-        self.full_node_ids = torch.arange(self.num_nodes+1).to(device)
+        self.full_node_ids = torch.arange(self.num_nodes + 1).to(device)
         self.gnn_hidden_dim = gnn_hidden_dim
 
-        self.prototypes = nn.Parameter(torch.randn(1, 1, proto_num, set_trans_out_dim))
+        self.prototypes = nn.Parameter(torch.randn(proto_num, set_trans_out_dim))
 
         if node_embed is None:
-            self.node_embed = nn.Embedding(num_nodes+1, start_embed_dim, padding_idx=0)
+            self.node_embed = nn.Embedding(num_nodes + 1, start_embed_dim, padding_idx=0)
         '''
         else:
             self.node_embed = nn.Embedding.from_pretrained(node_embed, freeze=freeze)
@@ -136,12 +151,9 @@ class OurModel(nn.Module):
 
         self.time_trans_embed = DataEmbedding(num_visits, max_weeks_between, proto_out_dim)
 
-        self.lin = nn.Linear(start_embed_dim, gnn_hidden_dim)
-        self.head = nn.Linear(time_trans_out_dim, out_dim)
+        self.fc = nn.Linear(start_embed_dim, gnn_hidden_dim)
 
-        self.gnn = nn.ModuleDict()
-        for layer in range(1, gnn_layer + 1):
-            self.gnn[str(layer)] = GATConv(gnn_hidden_dim, gnn_hidden_dim)
+        self.gnn = nn.ModuleList([GATConv(gnn_hidden_dim, gnn_hidden_dim) for _ in range(gnn_layer)])
 
         self.set_transformer = SetTransformer(dim_input=gnn_hidden_dim,
                                               num_outputs=set_trans_out_num,
@@ -158,40 +170,55 @@ class OurModel(nn.Module):
                                                   num_heads=proto_head_num,
                                                   depth=proto_depth)
 
-    def forward(self, node_ids, edge_idx, edge_attr, visit_times, visit_order):
+        self.time_transformer = TimeTransformer(hidden_dim=time_trans_hidden_dim,
+                                                num_heads=time_head_num,
+                                                depth=time_depth)
 
-        print(f'node id shape: {node_ids.shape}')
-        print(f'edge idx shape: {edge_idx.shape}')
-        print(f'edge attr shape: {edge_attr.shape}')
+        self.head = PredictionHead(attn_hidden_dim=2 * time_trans_hidden_dim,
+                                   mlp_input_dim=2 * time_trans_hidden_dim,
+                                   mlp_hidden_dim=2 * time_trans_hidden_dim,
+                                   mlp_output_dim=out_dim)
+
+    def forward(self, node_ids, edge_idx, edge_attr, visit_times, visit_order, attn_mask):
+
+        # print(f'node id shape: {node_ids.shape}')
+        # print(f'edge idx shape: {edge_idx.shape}')
+        # print(f'edge attr shape: {edge_attr.shape}')
 
         # full_edge_ids = torch.arange(self.num_edges)
         # edge_embed = self.edge_embed(full_edge_ids)
-        # edge_embed = self.lin(edge_embed)        
+        # edge_embed = self.lin(edge_embed)
 
-        x = self.node_embed(self.full_node_ids)     # (node_num,D)
-        x = self.lin(x)
+        x = self.node_embed(self.full_node_ids)  # (node_num,D)
+        # x = self.fc(x)
 
         # Global GNN
-        for layer in range(1, self.gnn_layer + 1):
-            x = self.gnn[str(layer)](x, edge_idx, edge_attr=edge_attr)
+        for layer in self.gnn:
+            x = layer(x, edge_idx, edge_attr=edge_attr)
 
         B, V, N = node_ids.shape
         x = x.unsqueeze(0).unsqueeze(0).expand(B, V, -1, -1)
+        # TODO double check
         x = torch.gather(x, 2, node_ids.unsqueeze(-1).expand(-1, -1, -1, self.gnn_hidden_dim))
 
-        zero_elements = node_ids == 0
-        zero_rows = zero_elements.all(dim=-1)
-
         # Set Transformer
-        x = self.set_transformer(x, attn_mask=zero_rows)  # (B,V,1,D)
+        x = self.set_transformer(x, attn_mask=attn_mask)  # (B,V,1,D)
+        visit_x = x
 
         # Learn Prototypes
-        prototypes = self.prototypes.repeat(B, V, 1, 1)  # (B,V,proto_num,D)
-        x = self.prototype_learner(x, prototypes)
+        prototypes = self.prototypes.unsqueeze(0).unsqueeze(0)
+        prototypes = prototypes.repeat(B, V, 1, 1)  # (B,V,proto_num,D)
+        x = self.prototype_learner(x, prototypes)  # (B,V,1,D)
 
         # Pos and Time Embedding
-        embed = self.time_trans_embed(visit_order, visit_times)
-        embed = embed.unsqueeze(-2)
+        embed = self.time_trans_embed(visit_order, visit_times)  # (B,V,D)
 
         # Time-Aware Transformer
-        
+        x = x.squeeze(-2)
+        visit_x = visit_x.squeeze(-2)
+        x = self.time_transformer(x, visit_x, embed, attn_mask=attn_mask)
+
+        # Prediction
+        out = self.head(x)
+
+        return out, self.prototypes
--- model/modules.py
+++ model/modules.py
@@ -4,6 +4,32 @@ import torch.nn.functional as F
 import math
 
 
+class MLP(nn.Module):
+
+    def __init__(self,
+                 input_dim: int,
+                 hidden_dim: int = None,
+                 output_dim: int = None,
+                 act_layer=nn.GELU,
+                 dropout_prob: float = 0.):
+        super().__init__()
+
+        output_dim = output_dim or input_dim
+        hidden_dim = hidden_dim or input_dim
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.drop = nn.Dropout(dropout_prob)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
 class PositionalEmbedding(nn.Module):
 
     def __init__(self, embed_num, embed_dim):
@@ -12,25 +38,24 @@ class PositionalEmbedding(nn.Module):
         self.pos_embed = nn.Embedding(embed_num, embed_dim)
 
     def forward(self, visit_order):
-        
+
         return self.pos_embed(visit_order)
 
 
 class TemporalEmbedding(nn.Module):
 
-    def __init__(self,
-                 embed_num,
-                 embed_dim):
+    def __init__(self, embed_num, embed_dim):
         super().__init__()
 
         self.time_embed = nn.Embedding(embed_num, embed_dim, padding_idx=0)
 
     def forward(self, visit_rel_times):
-        
+
         return self.time_embed(visit_rel_times)
 
 
 class DataEmbedding(nn.Module):
+
     def __init__(self, pos_embed_num, time_embed_num, embed_dim, dropout=0.1):
         super().__init__()
 
@@ -41,8 +66,8 @@ class DataEmbedding(nn.Module):
 
     def forward(self, visit_order, visit_rel_times):
         embed = self.pos_embed(visit_order) + self.time_embed(visit_rel_times)
-        
-        return embed # self.dropout(embed)
+
+        return embed  # self.dropout(embed)
 
 
 class MAB(nn.Module):
@@ -64,27 +89,53 @@ class MAB(nn.Module):
         Q = self.fc_q(Q)
         K, V = self.fc_k(K), self.fc_v(K)
 
-        last_dim = len(Q.shape)-1
+        if attn_mask is not None:
+            # attn_mask: (B,V,N)
+            if K.dim() == 3:
+                attn_mask = attn_mask.all(dim=-1)  # Visit Mask (B,V)
+
+            if Q.size(-2) == attn_mask.size(-1):
+                Q.masked_fill_(attn_mask.unsqueeze(-1), value=0)
+            if K.size(-2) == attn_mask.size(-1):
+                K.masked_fill_(attn_mask.unsqueeze(-1), value=0)
+                V.masked_fill_(attn_mask.unsqueeze(-1), value=0)
 
         dim_split = self.dim_V // self.num_heads
-        Q_ = torch.cat(Q.split(dim_split, last_dim), 0)
-        K_ = torch.cat(K.split(dim_split, last_dim), 0)
-        V_ = torch.cat(V.split(dim_split, last_dim), 0)
+        Q_ = torch.cat(Q.split(dim_split, -1), dim=0)
+        K_ = torch.cat(K.split(dim_split, -1), dim=0)
+        V_ = torch.cat(V.split(dim_split, -1), dim=0)
 
-        _, _, nq, _ = Q_.shape
-        bh, v, nk, _ = K_.shape
+        scores = Q_.matmul(K_.transpose(-2, -1)) / math.sqrt(dim_split)
 
-        scores = Q_.matmul(K_.transpose(2, 3)) / math.sqrt(dim_split)
+        # if attn_mask is not None:
+        #     nq = Q_.size(-2)
+        #     bh = K_.size(0)
+        #     visit_attn_mask = attn_mask.all(dim=-1)
+        #     visit_attn_mask = visit_attn_mask.repeat(self.num_heads, 1)
 
-        if attn_mask is not None:
-            # attn_mask (B, V) -> (BH, V, NQ, NK)
-            attn_mask = attn_mask.repeat(self.num_heads, 1)
-            attn_mask = attn_mask.unsqueeze(-1).unsqueeze(-1)
-            attn_mask = attn_mask.expand(bh, v, nq, nk)
-            scores = scores.masked_fill(attn_mask == True, float('-inf'))
+        #     if K.dim() == 3:
+        #         # visit_attn_mask (B, V) -> (BH, NQ, NK)
 
-        A = torch.softmax(scores, last_dim)
-        O = torch.cat((Q_ + A.matmul(V_)).split(Q.size(0), 0), last_dim)
+        #         if Q.size(-2) == attn_mask.size(-1):
+        #             Q_mask = visit_attn_mask.unsqueeze(-1)
+        #             Q_mask = Q_mask.expand(bh, nq, nk)
+
+        #         score_mask = visit_attn_mask.expand(bh, )
+
+        #         visit_attn_mask = visit_attn_mask.unsqueeze(-2)
+        #         bh, nk = K_.size(0), K_.size(-2)
+        #         visit_attn_mask = visit_attn_mask.expand(bh, nq, nk)
+
+        #     elif K.dim() == 4:
+        #         # visit_attn_mask (B, V) -> (BH, V, NQ, NK)
+        #         visit_attn_mask = visit_attn_mask.unsqueeze(-1).unsqueeze(-1)
+        #         bh, v, nk = K_.size(0), K_.size(1), K_.size(-2)
+        #         visit_attn_mask = visit_attn_mask.expand(bh, v, nq, nk)
+
+        #     scores = scores.masked_fill(visit_attn_mask == True, float('-inf'))
+
+        A = torch.softmax(scores, dim=-1)
+        O = torch.cat((Q_ + A.matmul(V_)).split(Q.size(0), 0), dim=-1)
         O = O if getattr(self, 'ln0', None) is None else self.ln0(O)
         O = O + F.relu(self.fc_o(O))
         O = O if getattr(self, 'ln1', None) is None else self.ln1(O)
