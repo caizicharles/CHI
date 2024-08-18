216e6e030db5537e5955f9220462bb1d762f39dc
Zi Cai <98086762+caizicharles@users.noreply.github.com>
2024-08-18 15:20:13 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ conda create -n "{env_name}" python=3.9.16
 
 conda install numpy
 pip install torch==1.12.0
+pip install torch-scatter
 pip install torch-geometric==2.3.0
 pip install pyhealth==1.1.2
 pip install scikit-learn==1.2.1
